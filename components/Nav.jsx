6e2e7960a0a3af87998c2c8c06515b5da92fac79
surajshukla <surajshukla5604@gmail.com>
2023-08-12 23:45:32 +0530

    fixing image bug on deployment - 1

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
+import logo from 'Public/assets/images/logo.svg';
+
 const Nav = () => {
   const { data: session } = useSession();
 
@@ -30,7 +32,7 @@ const Nav = () => {
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src='/assets/images/logo.svg'
+          src={logo}
           alt='logo'
           width={30}
           height={30}
